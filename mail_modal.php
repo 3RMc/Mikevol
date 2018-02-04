a8329d7b18ffa7c6429eba006f3232654c14ee65
3RMc <ermakoff1994@mail.ru>
2018-02-04 11:24:58 +0300

    Шаблон и модальное окно письма

---
+++ mail_modal.php
@@ -0,0 +1,49 @@
+   
+    
+<div class="modal fade" id="myModal7" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+ 	<div class="modal-dialog" role="document">
+   		<div style="margin-top: 0; background-color: transparent; color: #6e0314;" class="modal-content">
+   			<div id="form123">
+				<div class="row">
+					<div class="col-xs-12">
+							<header class="text-center">
+								Оставьте заявку <span>на кухню своей мечты</span> прямо сейчас:
+							</header>
+						<form  action="thanks.php" id="formB" method="post" name="application">
+							<div class="form-group form-group-lg">
+								<input required class="form-control" name="inputFIO" placeholder="Имя"> 
+								<input required class="form-control" id="inputTel" name="inputTel" placeholder="Телефон">  
+							</div>
+							<button type="submit" id="but3" type="button" class="btn btn-default upper">оставить заявку</button>
+						</form>
+					</div>
+				</div>
+			</div>
+   		</div>
+ 	</div>
+</div> 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
